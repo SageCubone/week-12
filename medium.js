4142982a7cef9a7fcc27f4393abfa1181ef7bf12
jorge <jorgealejandrovaca@gmail.com>
2020-12-08 22:28:59 -0500

    first commit

---
+++ medium.js
@@ -0,0 +1,3 @@
+// I was confused on how this supposed to be done. 
+console.log('mercury, venus, earth, mars, jupiter, saturn ,uranus, neptune, pluto');
+// Run this on the terminal to print the planets (node medium.js)
